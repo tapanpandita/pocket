1ab2f58d25118be5bd86876c5f01ca6f86b326b1
Tapan Pandita <tapan.pandita@gmail.com>
2012-08-11 22:59:54 +0530

    added docstrings and some minor bugfixes

--- pocket.py
+++ pocket.py
@@ -8,13 +8,19 @@ except ImportError:
 def method_wrapper(fn):
     @wraps(fn)
     def wrapped(self, *args, **kwargs):
-        payload = dict([(k, v) for k, v in kwargs.iteritems() if v])
+        payload = dict([(k, v) for k, v in kwargs.iteritems() if v == None])
         payload.update(self._payload)
         r = requests.post(self.api_endpoints[fn.__name__], data=payload)
         if r.status_code > 399:
             error_msg = self.statuses.get(r.status_code)
             extra_info = r.headers.get('X-Error')
-            ExceptionClass = type('%sException' % fn.__name__.capitalize(),
+            exceptions = {
+                400: 'InvalidQuery',
+                401: 'Auth',
+                403: 'RateLimit',
+                503: 'ServerMaintenance',
+            }
+            ExceptionClass = type('%sException' % exceptions.get(r.status_code, 'Pocket'),
                 (Exception, ),
                 {}
             )
@@ -23,6 +29,9 @@ def method_wrapper(fn):
     return wrapped
 
 class Pocket:
+    '''
+    This class implements a basic python wrapper around the pocket api. For a detailed documentation of the methods and what they do please refer the official pocket api documentation at http://getpocket.com/api/docs
+    '''
     def __init__(self, username, password, api_key):
         self.username = username
         self.password = password
@@ -51,26 +60,49 @@ class Pocket:
 
     @method_wrapper
     def add(self, url, title=None, ref_id=None):
+        '''
+        This method allows you to add a page to a user's list.
+        '''
         pass
 
+    @method_wrapper
     def send(self, url_map=None, read_map=None, update_title=None, update_tags=None):
+        '''
+        This method allows you to make changes to a user's list. It supports adding new pages, marking pages as read, changing titles, or updating tags. Multiple changes to items can be made in one request.
+        '''
         pass
 
-    def get(self, format, state, myAppOnly, since, count, page, tags):
+    @method_wrapper
+    def get(self, format='json', state=None, myAppOnly=None, since=None, count=None, page=None, tags=None):
+        '''
+        This method allows you to retrieve a user's list. It supports retrieving items changed since a specific time to allow for syncing.
+        '''
         pass
 
     @method_wrapper
     def stats(self, format='json'):
+        '''
+        This method returns some information about a user's list.
+        '''
         pass
 
     @method_wrapper
     def auth(self):
+        '''
+        The authentication method is used to verify a supplied username and password is correct (for example when prompting a user for their credentials for the first time). It does not need to be called before using any methods below.
+        '''
         pass
 
     @method_wrapper
     def signup(self):
+        '''
+        This method allows you to create a new user account.
+        '''
         pass
 
     @method_wrapper
     def api(self):
+        '''
+        This method allows you to check your current rate limit status. Calls to this method do not count against the rate limit.
+        '''
         pass
