643093418f72e8dba65ce000317d12d286003d59
Tapan Pandita <tapan.pandita@gmail.com>
2012-08-11 20:49:28 +0530

    added methods to pocket

--- pocket.py
+++ pocket.py
@@ -10,39 +10,50 @@ class Pocket:
         self.username = username
         self.password = password
         self.api_key = api_key
-        self.api_endpoints = {
-            'add': 'https://readitlaterlist.com/v2/add',
-        }
-        self.statuses = {
-            200: 'Request was successful',
-            400: 'Invalid request, please make sure you follow the documentation for proper syntax',
-            401: 'Username and/or password is incorrect',
-            403: 'Rate limit exceeded, please wait a little bit before resubmitting',
-            503: 'Read It Later\'s sync server is down for scheduled maintenance',
-        }
-
-    def _query_builder(self, **kwargs):
-        d = {}
-        for k, v in kwargs.iteritems():
-            if v:
-                d[k] = v
-        return urllib.urlencode(d)
-
-    def add(self, url, title=None, ref_id=None):
-        query = self._query_builder(**{
+        self._payload = {
             'apikey': self.api_key,
             'username': self.username,
             'password': self.password,
+        }
+        self.api_endpoints = {
+            'add': 'https://readitlaterlist.com/v2/add',
+            'send': 'https://readitlaterlist.com/v2/send',
+            'auth': 'https://readitlaterlist.com/v2/auth',
+        }
+        self.statuses = {
+            200: 'Request was successful.',
+            400: 'Invalid request, please make sure you follow the documentation for proper syntax.',
+            401: 'Username and/or password is incorrect.',
+            403: 'Rate limit exceeded, please wait a little bit before resubmitting.',
+            503: 'Read It Later\'s sync server is down for scheduled maintenance.',
+        }
+
+    def add(self, url, title=None, ref_id=None):
+        payload = {
             'url': url,
             'title': title,
             'ref_id': ref_id,
-        })
-        r = requests.get('%s?%s' % (self.api_endpoints['add'], query))
-        print r.content
+        }
+        payload.update(self._payload)
+        r = requests.post(self.api_endpoints['add'], data=payload)
         if r.status_code > 399:
             error_msg = self.statuses.get(r.status_code)
-            if error_msg:
-                print r.headers.get('X-Error')
-                raise Exception(error_msg)
-            else:
-                raise Exception(r.headers.get('X-Error'))
+            extra_info = r.headers.get('X-Error')
+            raise Exception('%s %s' % (error_msg, extra_info))
+        print r.content
+        return self
+
+    def send(self, url_map=None, read_map=None, update_title=None, update_tags=None):
+        pass
+
+    def get_list(self, format, state, myAppOnly, since, count, page, tags):
+        pass
+
+    def auth(self):
+        r = requests.post(self.api_endpoints['auth'], data=self._payload)
+        if r.status_code > 399:
+            error_msg = self.statuses.get(r.status_code)
+            extra_info = r.headers.get('X-Error')
+            raise Exception('%s %s' % (error_msg, extra_info))
+        print r.content
+        return self
