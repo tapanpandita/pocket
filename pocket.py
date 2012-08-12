2b661d9d90203a1419d6c2587e9d17b6d1e99c7e
Amit Upadhyay <comments@amitu.com>
2012-08-12 05:33:18 -0400

    added setup.py and few formatting, and a sanity related fix

--- pocket.py
+++ pocket.py
@@ -3,7 +3,20 @@ from functools import wraps
 try:
     import json
 except ImportError:
-    import simplejson as json
+    import simplejson as json # this is too old now
+
+class PocketException(Exception): pass
+class InvalidQueryException(PocketException): pass
+class AuthException(PocketException): pass
+class RateLimitException(PocketException): pass
+class ServerMaintenanceException(PocketException): pass
+
+EXCEPTIONS = {
+    400: InvalidQueryException,
+    401: AuthException,
+    403: RateLimitException,
+    503: ServerMaintenanceException,
+}
 
 def method_wrapper(fn):
     @wraps(fn)
@@ -14,48 +27,37 @@ def method_wrapper(fn):
         if r.status_code > 399:
             error_msg = self.statuses.get(r.status_code)
             extra_info = r.headers.get('X-Error')
-            exceptions = {
-                400: 'InvalidQuery',
-                401: 'Auth',
-                403: 'RateLimit',
-                503: 'ServerMaintenance',
-            }
-            ExceptionClass = type('%sException' % exceptions.get(r.status_code, 'Pocket'),
-                (Exception, ),
-                {}
+            raise EXCEPTIONS.get(r.status_code, PocketException)(
+                '%s %s' % (error_msg, extra_info)
             )
-            raise ExceptionClass('%s %s' % (error_msg, extra_info))
         return r.json or r.text, r.headers
     return wrapped
 
-class Pocket:
+class Pocket(object):
     '''
-    This class implements a basic python wrapper around the pocket api. For a detailed documentation of the methods and what they do please refer the official pocket api documentation at http://getpocket.com/api/docs
+    This class implements a basic python wrapper around the pocket api. For a
+    detailed documentation of the methods and what they do please refer the
+    official pocket api documentation at http://getpocket.com/api/docs
     '''
     def __init__(self, username, password, api_key):
-        self.username = username
-        self.password = password
-        self.api_key = api_key
         self._payload = {
             'apikey': self.api_key,
             'username': self.username,
             'password': self.password,
         }
-        self.api_endpoints = {
-            'add': 'https://readitlaterlist.com/v2/add',
-            'send': 'https://readitlaterlist.com/v2/send',
-            'get': 'https://readitlaterlist.com/v2/get',
-            'stats': 'https://readitlaterlist.com/v2/stats',
-            'auth': 'https://readitlaterlist.com/v2/auth',
-            'signup': 'https://readitlaterlist.com/v2/signup',
-            'api': 'https://readitlaterlist.com/v2/api',
-        }
+        self.api_endpoints = dict(
+            (method, 'https://readitlaterlist.com/v2/%s' % method)
+            for method in "add,send,get,stats,auth,signup,api".split(",")
+        )
         self.statuses = {
             200: 'Request was successful.',
-            400: 'Invalid request, please make sure you follow the documentation for proper syntax.',
+            400: 'Invalid request, please make sure you follow the '
+                 'documentation for proper syntax.',
             401: 'Username and/or password is incorrect.',
-            403: 'Rate limit exceeded, please wait a little bit before resubmitting.',
-            503: 'Read It Later\'s sync server is down for scheduled maintenance.',
+            403: 'Rate limit exceeded, please wait a little bit before '
+                 'resubmitting.',
+            503: "Read It Later's sync server is down for scheduled "
+                 "maintenance."
         }
 
     @method_wrapper
@@ -63,46 +65,51 @@ class Pocket:
         '''
         This method allows you to add a page to a user's list.
         '''
-        pass
 
     @method_wrapper
-    def send(self, url_map=None, read_map=None, update_title=None, update_tags=None):
+    def send(
+        self, url_map=None, read_map=None, update_title=None, update_tags=None
+    ):
         '''
-        This method allows you to make changes to a user's list. It supports adding new pages, marking pages as read, changing titles, or updating tags. Multiple changes to items can be made in one request.
+        This method allows you to make changes to a user's list. It supports
+        adding new pages, marking pages as read, changing titles, or updating
+        tags. Multiple changes to items can be made in one request.
         '''
-        pass
 
     @method_wrapper
-    def get(self, format='json', state=None, myAppOnly=None, since=None, count=None, page=None, tags=None):
+    def get(
+        self, format='json', state=None, myAppOnly=None, since=None,
+        count=None, page=None, tags=None
+    ):
         '''
-        This method allows you to retrieve a user's list. It supports retrieving items changed since a specific time to allow for syncing.
+        This method allows you to retrieve a user's list. It supports
+        retrieving items changed since a specific time to allow for syncing.
         '''
-        pass
 
     @method_wrapper
     def stats(self, format='json'):
         '''
         This method returns some information about a user's list.
         '''
-        pass
 
     @method_wrapper
     def auth(self):
         '''
-        The authentication method is used to verify a supplied username and password is correct (for example when prompting a user for their credentials for the first time). It does not need to be called before using any methods below.
+        The authentication method is used to verify a supplied username and
+        password is correct (for example when prompting a user for their
+        credentials for the first time). It does not need to be called before
+        using any methods below.
         '''
-        pass
 
     @method_wrapper
     def signup(self):
         '''
         This method allows you to create a new user account.
         '''
-        pass
 
     @method_wrapper
     def api(self):
         '''
-        This method allows you to check your current rate limit status. Calls to this method do not count against the rate limit.
+        This method allows you to check your current rate limit status. Calls
+        to this method do not count against the rate limit.
         '''
-        pass
